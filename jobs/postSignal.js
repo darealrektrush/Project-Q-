bb37c3d47593bbd77f7dedaeb8af50ff3d960adc
darealrektrush <darealrektrush@gmail.com>
2026-08-18 22:15:11 -0700

    Harden production recovery: update jobs/postSignal.js

--- jobs/postSignal.js
+++ jobs/postSignal.js
@@ -1,8 +1,26 @@
 import 'dotenv/config';
 import { createAndPostSignal } from '../src/lib/signal.js';
+import { requireEnv, signalsEnabled } from '../src/lib/featureFlags.js';
 
-createAndPostSignal()
+async function main() {
+  if (!signalsEnabled()) {
+    console.log('Signal posting disabled: PROJECT_Q_SIGNALS_ENABLED is not true.');
+    return null;
+  }
+
+  requireEnv([
+    'SUPABASE_URL',
+    'SUPABASE_SERVICE_ROLE_KEY',
+    'TELEGRAM_BOT_TOKEN',
+    'TELEGRAM_CHAT_ID',
+  ]);
+
+  return createAndPostSignal();
+}
+
+main()
   .then((row) => {
+    if (!row) return;
     console.log(`Posted signal ${row.id} (${row.kind})`);
   })
   .catch((err) => {
